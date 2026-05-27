4ce0e65897c3be3bb469b99c36947869b7a9da87
izan <104464981+Froallo@users.noreply.github.com>
2026-05-27 18:23:34 +0200

    Update config.js

--- styles/js/config.js
+++ styles/js/config.js
@@ -16,7 +16,7 @@ let charadex = {};
 /* ==================================================================== */
 charadex.site = {
   title: "NHK",
-  url: "https://charadex-team.github.io/charadex-v1.0/",
+  url: "https://froallo.github.io/NHK/",
   description: `NHK`
 }
 
