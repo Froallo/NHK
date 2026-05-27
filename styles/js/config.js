162a5040d0069aa88d1091263253a115944a5447
izan <104464981+Froallo@users.noreply.github.com>
2026-05-27 17:43:53 +0200

    Update config.js

--- styles/js/config.js
+++ styles/js/config.js
@@ -15,9 +15,9 @@ let charadex = {};
 /* Any preview links will still show Charadex's information
 /* ==================================================================== */
 charadex.site = {
-  title: "Charadex",
+  title: "NHK",
   url: "https://charadex-team.github.io/charadex-v1.0/",
-  description: `A tool for organizing small ARPGs and species.`
+  description: `NHK`
 }
 
 /* ==================================================================== */
